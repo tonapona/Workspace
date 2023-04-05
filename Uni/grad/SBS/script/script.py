8f1703cffcbf3deb2bcb75a9d8e9ade32ba68293
Martin Simak <martin.simaak@gmail.com>
2023-04-05 03:00:47 +0200

    SBS script used for a real path, ANT question set created

--- Uni/grad/SBS/script/script.py
+++ Uni/grad/SBS/script/script.py
@@ -3,22 +3,22 @@ import pandas as pd
 import urllib
 import urllib3
 import geopy.distance
-from numpy import sqrt
+from numpy import sqrt, log10, pi
 from math import ceil
 from time import sleep
 
 ## Dictionary of possible transmitters: [x, y, max_antenna_height]
 # max_antenna_height = 10 for unknown values
 TRANSMITTERS = {
-    "Praha Slivenec": [50.02011399999999, 14.34015700000002, 10],
+    "Praha-Slivenec": [50.02011399999999, 14.34015700000002, 10],
     "Bubovice": [49.9724979999999, 14.17755099999998, 10],
     "Zlicin": [50.05122599999999, 14.28301700000001, 10],
-    "Rudna": [50.018478, 14.19571300000003, 10],
-    "Chyne": [50.018478, 14.19571300000003, 10],
-    "Strahov": [50.07985277777778, 14.37583333333333, 60],
-    "Beroun-Zavodi": [49.97116999999999, 14.111799, 10],
-    "FEL": [50.103153, 14.392759, 40],
-    "Beroun-Mestska hora": [49.9626386, 14.0650983, 14],
+    "Rudna": [50.0186111, 14.1958333, 10],
+    "Chyne": [50.0523211, 14.2266106, 10],
+    "Strahov": [50.0797222, 14.3758333, 60],
+    "Zahrabska": [49.9711111, 14.1116667, 10],
+    "FEL": [50.1024983, 14.3927758, 40],
+    "Beroun": [49.9627778, 14.0650000, 14],
 }
 
 ### Simple persistent request function
@@ -39,17 +39,18 @@ def get_elevation(point):
     return response.json()['results'][0]['elevation']
 
 ### Function for calculating and printing path-clearance parameters
-def path_clearance(transmitter1, transmitter2):
+def path_clearance(transmitter1, transmitter2, Txh = 0):
     ## Initialization
     start = TRANSMITTERS[transmitter1]
     stop =  TRANSMITTERS[transmitter2]
     Rx = transmitter2
-    if get_elevation(start[0:2]) > get_elevation(stop[0:2]): start, stop = stop, start, Rx = transmitter1
+    if get_elevation(start[0:2]) > get_elevation(stop[0:2]): start, stop, Rx = stop, start, transmitter1
+    if Txh == 0: Txh = start[2]
 
     ## Inspect the path of the radio link for elevation every 100 meters
     d = geopy.distance.geodesic(start[0:2], stop[0:2])
     number_of_points = ceil(d.m/100)
-    # number_of_points = 10     # small number of points for debugging
+    # number_of_points = 20     # small number of points for debugging
 
     lat = []
     lon = []
@@ -78,27 +79,57 @@ def path_clearance(transmitter1, transmitter2):
     assert d - (d1 + d2) < 1e-04                            # adding the previous two should yield overall
     F1 = 17.3*sqrt((d1.km*d2.km)/(f*d.km))
 
-    ## Calculate the subrefraction height correction x
-    R_Z = 6371000                                           # mean value of the Earth's radius in m
+    ## Calculate the subrefraction height correction x for normal atmosphere (k = 4/3)
+    R_Z = 6371e3                                            # mean value of the Earth's radius in m
     k_e = 4/3                                               # value of the subrefractive factor k for normal atmosphere
     R_e = k_e*R_Z                                           # effective value of Earth's radius due to subrefraction
     x = (d1.m*d2.m)/(2*R_e)
-
-    ## Obtain antenna height at stop
-    h1 = float(elevations[0]) + start[2]                    # fixed antenna height in start including terrain elevation
+    ## Obtain antenna height Rxh1 for full F1 clearance
+    h1 = float(elevations[0]) + Txh                         # fixed antenna height in start including terrain elevation
     h0 = float(elevations[index_max])                       # maximum terrain elevation along the path
-    Rxh = d.m*(h0 + x + F1 - h1)/d1.m + h1 - float(elevations[-1])
+    Rxh1 = d.m*(h0 + x + F1 - h1)/d1.m + h1 - float(elevations[-1])
+
+    ## Calculate the subrefraction height correction x for the path length in question
+    k_e = 157/(144+2670/d.m)
+    R_e = k_e*R_Z
+    x = (d1.m*d2.m)/(2*R_e)
+    ## Obtain antenna height Rxh2 for temperature climate with path obstriction extended along a portion of the path (0.3*F1)
+    Rxh2 = d.m*(h0 + x + 0.3*F1 - h1)/d1.m + h1 - float(elevations[-1])
+
+    ## Choose the larger of the antenna heights
+    Rxh = max(Rxh1, Rxh2)
+
+    ## Estimate diffraction loss (valid for losses greater than 15 dB) due to the path in question
+    h = float(elevations[0]) + index_max*(float(elevations[-1]) - float(elevations[0]))/number_of_rows - h0
+    Ad = -20*h/F1 + 10
+
+    ## Free space loss (FSL)
+    Gr = 44
+    Gt = 44
+    c = 3e8
+    FSL = 20*log10(4*pi*f*1e9*d.m/c) - Gr - Gt
+
+    ## Attenuation due to atmospheric gases
+    # gamma should be obtained according to ITU-R P.676
+    gamma = 0.097
+    Aa = gamma*d.km
 
     ## Output the results into a file
     with open(fr"C:\\Workspace\\Uni\\grad\\SBS\\script\\{transmitter1}_{transmitter2}_{number_of_points}points.txt", 'w') as file:
         file.write('*'*20 + "PATH-CLEARANCE" + '*'*20 + "\n")
         file.write("Point of maximum elevation h0: [" + point_max[0] + "," + point_max[1] + "]\n")
-        file.write("Parameters: d = " + str(round(d.km,2)) +" km, d1 = " + str(round(d1.km,2)) + " km, d2 = " + str(round(d2.km,2)) + " km, h1 = (" + str(round(float(elevations[0]))) + " + " + str(round(start[2])) + ") m, h0 = " + str(round(h0,2)) + " m, F1 = " + str(round(F1,2)) + " m, x = " + str(round(x,2)) + " m\n")
+        file.write("Parameters: d = " + str(round(d.km,2)) + " km, d1 = " + str(round(d1.km,2)) + " km, d2 = " + str(round(d2.km,2)) + " km, h1 = (" + str(round(float(elevations[0]))) + " + " + str(round(start[2])) + ") m, h0 = " + str(round(h0,2)) + " m, F1 = " + str(round(F1,2)) + " m, x = " + str(round(x,2)) + " m\n")
         file.write("Height of the " + Rx + " antenna required for line-of-sight path clearance: Rxh = " + str(round(Rxh,2)) + " m\n")
+        file.write("Corresponding losses: Ad = " + str(round(Ad, 2)) + " dB, FSL = " + str(round(FSL, 2)) + " dB, Aa = " + str(round(Aa, 2)) + " dB\n")
         file.write("\n" + '*'*20 + "DATA" + '*'*20 + '\n' + data_csv)
+
+    return Rxh
 
 
 ### Main function
 if __name__ == "__main__":
     ## Path clearance function with user input
-    path_clearance("Strahov", "Beroun-Zavodi")
+    path_clearance("FEL", "Strahov")
+    Rxh = path_clearance("Strahov", "Rudna")
+    Rxh = path_clearance("Rudna", "Zahrabska", Rxh)
+    path_clearance("Zahrabska", "Beroun", Rxh)
